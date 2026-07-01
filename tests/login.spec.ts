353b6b13f7b5accca695d61f94462c48df71ca70
Ishan Mittal <ishanmittal120@gmail.com>
2026-07-01 15:14:15 +0530

    chore(test): create login test file (part 1)

---
+++ tests/login.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test'
+
+// Login tests - Email & Password
+test.describe('Login - Email & Password', () => {
+  // Test cases will be appended incrementally
+})
